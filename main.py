e4fd85a9926c5b398db8d2da082dee49dd4e5362
Farmonov Hayitmurod <fhayitmurods@gmail.com>
2025-02-17 14:00:08 +0500

    juft sonlar yig'indisi

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ nums = list(map(int, input("son kiriting space bilan >>> ").split()))
 
 nums_t = sorted(nums, key=lambda x: 1 if x % 2 == 0 else 0)
 nums_t_ = sorted(nums, key=lambda x: x + 100  if x % 2 == 0 else x)
-print(*nums_t_)
+print("bu juf", *nums_t_)
 print(*nums_t)
 
+
+sum_juft = sum(x for x in nums if x % 2 == 0)
+print(sum_juft)
