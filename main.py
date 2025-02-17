1299c979bd734b7368339e2436523a727bc80b56
Farmonov Hayitmurod <fhayitmurods@gmail.com>
2025-02-17 17:05:17 +0500

    fix bag chenging main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ sum_juft = sum(x for x in nums if x % 2 == 0)
 print(sum_juft)
 
 print("hello world")
-print((i for i in range(10000000000000)))
+print((i+1 for i in range(10000000000000)))
 
 """
 ----------------------------salom----------------------------
